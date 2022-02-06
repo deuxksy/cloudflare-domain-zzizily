82e9575c74b2e3c55eb1c6368d7c4889a1cb6b57
SeokYoung.Kim <deuxksy@gmail.com>
2022-02-06 20:26:56 +0900

    ci/cd: tf log trace

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "remote" {
     organization = "ZZiZiLY"
-    token        = "FSN2md9UujIl0A.atlasv1.Ak2Q5T2c25hzlfUB42DYu3mvCYZYXABjRWfRxN81uPQrS4wP3riBUaHxy98FuMFtzzU"
     workspaces {
       name = "zzizilycom-cloudflare"
     }
