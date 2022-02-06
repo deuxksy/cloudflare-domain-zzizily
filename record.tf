9377b1adcce53b59e5192571233945c7b72ca6bc
SeokYoung.Kim <deuxksy@gmail.com>
2022-02-06 18:56:16 +0900

    feat: 이런 저런 domain 연동

--- record.tf
+++ record.tf
@@ -1,7 +1,16 @@
 resource "cloudflare_record" "local" {
-  zone_id = var.zone_id
+  zone_id = var.cloudflare_zone_id
   name    = "local"
   value   = "127.0.0.1"
   type    = "A"
+  # allow_overwrite = true
+}
+
+resource "cloudflare_record" "root" {
+  zone_id = var.cloudflare_zone_id
+  name    = ""
+  value   = "49.161.254.154"
+  type    = "A"
   proxied = true
+  # allow_overwrite = true
 }
