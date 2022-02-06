616e213fdd50c597164d7e7ff4bf0c484ef21956
SeokYoung.Kim <deuxksy@gmail.com>
2022-02-06 20:04:57 +0900

    ci/cd: tf init

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     organization = "ZZiZiLY"
-    token        = var.tf_login_token
+    token        = "FSN2md9UujIl0A.atlasv1.Ak2Q5T2c25hzlfUB42DYu3mvCYZYXABjRWfRxN81uPQrS4wP3riBUaHxy98FuMFtzzU"
     workspaces {
       name = "zzizilycom-cloudflare"
     }
