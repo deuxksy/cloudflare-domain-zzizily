21eacccc689d21d564728c2fee45fc72d2974f24
SeokYoung Kim <deuxksy@gmail.com>
2022-05-09 18:04:05 +0900

    Update record-a.tf

--- record-a.tf
+++ record-a.tf
@@ -6,14 +6,6 @@ resource "cloudflare_record" "local" {
   # allow_overwrite = true
 }
 
-resource "cloudflare_record" "mirror" {
-  zone_id = var.cloudflare_zone_id
-  name    = "local"
-  value   = "127.0.0.1"
-  type    = "A"
-  # allow_overwrite = true
-}
-
 resource "cloudflare_record" "nlog" {
   zone_id = var.cloudflare_zone_id
   type    = "A"
